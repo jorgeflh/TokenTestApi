4fd40c620713d26c96535f32a420eb6076e93640
Jorge Filho <jorgeflh@gmail.com>
2019-11-06 21:49:23 -0300

    Add Customer model

---
+++ TokenTestApi.Core/Domain/Models/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TokenTestApi.Core.Domain.Models
+{
+    public class Customer
+    {
+        public DateTime RegistrationDate { get; set; }
+        public long CardNumber { get; set; }
+        public int Cvv { get; set; }
+        public long Token { get; set; }
+    }
+}
