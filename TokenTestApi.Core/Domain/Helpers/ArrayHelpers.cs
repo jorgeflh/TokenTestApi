a28794c7f9e20b0fab6599a1673dae1731f640d3
Jorge Filho <jorgeflh@gmail.com>
2019-11-21 19:01:26 -0300

    Add helper with Absolute difference function

---
+++ TokenTestApi.Core/Domain/Helpers/ArrayHelpers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TokenTestApi.Core.Domain.Helpers
+{
+    public static class ArrayHelpers
+    {
+        public static int[] GetNumbersBasedOnAbsoluteDifference(int[] rawToken)
+        {            
+            var minNumber = rawToken.Min();
+            var myList = new List<int>();
+
+            for (var i = 0; i < rawToken.Length; i++)
+            {
+                if (Math.Abs(minNumber - rawToken[i]) <= 4)
+                {
+                    myList.Add(rawToken[i]);
+                }
+            }
+
+            return myList.ToArray();
+        }
+    }
+}
